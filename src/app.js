adf95f26cbdeb7dab9620b0c3656b2b41d291f6c
katherine <pv.katherine7@gmail.com>
2022-11-24 11:51:17 +0000

    deploy page

--- src/app.js
+++ src/app.js
@@ -1,3 +1,33 @@
 import "./style.scss";
+import confetti from "canvas-confetti";
+import ColorThief from "colorthief";
 
-console.log("Welcome to NPM!");
+const colorThief = new ColorThief();
+
+const triggerConfettiButton = document.querySelector(".confetti-button");
+const displayedImage = document.querySelector("#image");
+const imageInputBar = document.querySelector("#url-user-input");
+
+console.log(imageInputBar);
+
+const changeDisplayedImage = () => {};
+
+const fireConfetti = () => {
+  confetti({
+    particleCount: 100,
+    startVelocity: 30,
+    spread: 360,
+    origin: {
+      x: Math.random(),
+      // since they fall down, start a bit higher than random
+      y: Math.random() - 0.2,
+    },
+  });
+};
+
+// get colour from the image DOM Element
+// check if image has loaded
+// console.log what the colours are in the
+
+triggerConfettiButton.addEventListener("click", fireConfetti);
+imageInputBar.addEventListener("input", changeDisplayedImage);
